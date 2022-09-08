36542c20e8335abf2c06ea64287150ddaadc5b0c
Rodrigo Sakae <devsakae@gmail.com>
2022-09-08 16:11:17 -0300

    Requisito 01 cumprido e passado no teste

--- src/services/api.js
+++ src/services/api.js
@@ -3,13 +3,15 @@ export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(url);
   const objJason = await response.json();
+  console.log(objJason);
   return objJason;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY';
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const response = await fetch(url);
   const objJason = await response.json();
+  console.log(objJason);
   return objJason;
 }
 
