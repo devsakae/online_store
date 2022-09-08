497b794e94ed9680b142852b940d2c8ba5acd302
Leonardo <leonardoluni@gmail.com>
2022-09-08 16:36:20 -0300

    requisito 4

---
+++ src/pages/Categorias.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Categorias extends React.Components {
+  render() {
+    return (
+      <div>
+        <label htmlFor="categorias">
+          <input
+            type="radio"
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default Categorias;
