56ba85325a3a5d0c38d4be6bfab813a49ad77040
Rodrigo Sakae <devsakae@gmail.com>
2022-09-08 16:32:49 -0300

    Requisito 03 codado e testado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Search from './pages/Search';
+import Carrinho from './pages/Carrinho';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
+          <Link
+            to="/carrinho"
+            data-testid="shopping-cart-button"
+          >
+            🛒 Carrinho de compras
+          </Link>
           <Search
             searchString={ false }
           />
+        </Route>
+        <Route exact path="/carrinho">
+          <Carrinho />
         </Route>
       </Switch>
     </BrowserRouter>
