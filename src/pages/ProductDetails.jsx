3cd502867178f6904c0e7093d86069fb69337451
Rodrigo Sakae <devsakae@gmail.com>
2022-09-09 11:10:03 -0300

    Exibindo nome na tela do produto clicado

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { getProductById } from '../services/api';
 
 export default class ProductDetails extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    const getEm = async () => {
+      const { match: { params: { id } } } = this.props;
+      const productdetails = await getProductById(id);
+      this.setState({
+        loading: false,
+        productdetails,
+      });
+    };
+    getEm();
+  }
+
   render() {
+    const { productdetails, loading } = this.state;
     return (
-      <div>ProductDetails</div>
+      <>
+        <div>ProductDetails</div>
+        { !loading && productdetails.title }
+        teste
+      </>
     );
   }
 }
